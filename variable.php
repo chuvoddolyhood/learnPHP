01e14cc93e550aa1fb8c1917eb9db5a02c56697b
chuvoddolyhood <trannhannghia.24012000@gmail.com>
2021-08-19 15:02:34 +0700

    initial code

---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+    $var = 2;
+    echo "var = $var"  . "<br/>";
+    echo $var  . "<br/>";
+
+    $string_name = "Nghia";
+    echo "string_name: $string_name"  . "<br/>";
+    echo "Name: " . $string_name  . "<br/>";
+
+    //loop
+    $var = array(1,2,3,4);
+    for($i = 0; $i < 4; $i++){
+        echo $var[$i] . "<br/>";
+    }
+?>
